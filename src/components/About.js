0a3e91d1b5c7fadbfb7a27c6609c04196995af90
serah shani <serahshani035@gmail.com>
2024-08-01 13:52:33 +0300

    imports

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { image } from "../data/user";
+
 
 function About() {
   return (
@@ -7,5 +9,7 @@ function About() {
       <p>I Made This</p>
       <img src={image} alt="I made this" />
     </div>
+  
   );
 }
+export default About
